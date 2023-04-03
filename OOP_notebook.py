4f49193907bb5a49306a9c041657975dc0db5512
MJ-AllDay-67 <Michael.Tiernan.J@gmail.com>
2023-04-02 19:46:01 -0500

    data update

--- OOP_notebook.py
+++ OOP_notebook.py
@@ -16,22 +16,38 @@ class DataImport:
 ########### Import Data ###########
 object_1 = DataImport()
 df_batting = object_1.importcsv("Batting")
+df_fielding = object_1.importcsv("Fielding")
 df_people = object_1.importcsv("People")
 df_player_stats = object_1.importcsv("stats")
 df_hof = object_1.importcsv("HallOfFame")
 
-# print(df_teams.head())
-# print(df_gamelogs.head())
-# print(df_stats.head())
 
-# # drop cols
-# cols = ['teamID','lgID','divID','Rank','G','Ghome','L','CS','HBP','IPouts','name','park','attendance','teamIDBR','teamIDlahman45','teamIDretro']
-# df_teams.drop(cols, axis=1, inplace=True)
-# df_teams.tail()
+########### drop cols ###########
+# batting
+cols = ['stint','CS','IBB','HBP','SH','SF']
+df_batting.drop(cols, axis=1, inplace=True)
+print(df_batting.tail())
 
+#Fielding
+df_fielding = df_fielding[['playerID','yearID','teamID','POS']]
+print(df_fielding.tail())
 
+# people
+cols = ['birthYear', 'birthMonth', 'birthDay','birthCity','deathYear','deathMonth','deathDay','deathCountry','deathState','deathCity','nameGiven','debut',\
+    'finalGame','retroID','bbrefID']
+df_people.drop(cols, axis=1, inplace=True)
+print(df_people.tail())
 
+# player stats
+cols = ['isolated_power','b_lob','Unnamed: 14']
+df_player_stats.drop(cols, axis=1, inplace=True)
+print(df_player_stats.tail())
 
+# HOF
+print(df_hof.columns)
+cols = ['votedBy','ballots','needed','votes','needed_note']
+df_hof.drop(cols, axis=1, inplace=True)
+print(df_hof.tail())
 
 
 
